609ec37ed3a9a0f970061344c8511fd442ddf88b
Rithikadurai29 <durairithika26@gmail.com>
2026-06-11 15:51:57 +0530

    STUDENT

--- src/day7.java
+++ src/day7.java
@@ -1,4 +1,4 @@
-
+/*
 public class Day5 {
 
     static class Car {
@@ -52,5 +52,78 @@ public class Day5 {
         myCar.decelerate();
         myCar.refill();
         myCar.showFuel();
+    }
+}
+ */
+public class Day6_part {
+
+    static class Student {
+        String Name;
+        int RegNo;
+        String Department;
+        int MathsMark;
+        int PhysicsMark;
+        int CSEMark;
+
+        // Constructor n+r
+        void StudentInfo(String Name, int RegNo) {
+            this.Name = Name;
+            this.RegNo = RegNo;
+        }
+
+        // Constructor n+r+d
+        void information(String Name, int RegNo, String Department) {
+            this.Name = Name;
+            this.RegNo = RegNo;
+            this.Department = Department;
+        }
+
+        // Getters
+        int getMathsMark() {
+            return MathsMark;
+        }
+
+        int getPhysicsMark() {
+            return PhysicsMark;
+        }
+
+        int getCSEMark() {
+            return CSEMark;
+        }
+
+        // Method Overloading
+        void StudentMarks(int MathsMark) {
+            this.MathsMark = MathsMark;
+            System.out.println("Updating Maths Mark : " + MathsMark);
+        }
+
+        void StudentMarks(int PhysicsMark, int CSEMark) {
+            this.PhysicsMark = PhysicsMark;
+            this.CSEMark = CSEMark;
+            System.out.println("Updating Physics Mark : " + PhysicsMark);
+            System.out.println("Updating CSE Mark : " + CSEMark);
+        }
+
+        void display() {
+            System.out.println("Name : " + Name);
+            System.out.println("Reg No : " + RegNo);
+            System.out.println("Department : " + Department);
+            System.out.println("Maths Mark : " + MathsMark);
+            System.out.println("Physics Mark : " + PhysicsMark);
+            System.out.println("CSE Mark : " + CSEMark);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.StudentInfo("ASHMITHA", 2526105);
+        s1.information("rithika", 2526105, "AI%DS");
+
+        s1.StudentMarks(90);
+        s1.StudentMarks(85, 95);
+
+        s1.display();
     }
 }
