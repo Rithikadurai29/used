b474ef3e599e7ca9a93af6737771d05622f9728e
Rithika <durairithika26@gmail.com>
2026-06-16 15:02:13 +0530

    hello

--- src/Day10.java
+++ src/Day10.java
@@ -1,22 +1,64 @@
 import java.util.ArrayList;
 
 
-public class day10 {
+/*public class day10 {
     static void main() {
-        ArrayList<String>list = new ArrayList();
+        ArrayList<String> list = new ArrayList();
 
         list.add("Ashmi");
         list.add("Lazzi");
         list.add("Sajii");
         list.add("Rithi");
         list.add("dhesh");
-        for(String d: list) {
+        for (String d : list) {
             System.out.println(d);
         }
         list.remove("dhesh");
         System.out.println();
-        for(String d: list) {
+        for (String d : list) {
             System.out.println(d);
         }
     }
 }
+ */
+
+      import java.util.LinkedList;
+public class day10 {
+    static void main() {
+        LinkedList<String> queue = new LinkedList<String>();
+        queue.add("dhanu");
+        queue.add("Menaga");
+        queue.add("priya");
+        queue.add("Mithra");
+        queue.add("Nivetha");
+        while(!queue.isEmpty()) {
+            process(queue);
+        }
+    }
+    static void process(LinkedList<String> queue){
+        String removed=queue.removeFirst();
+        System.out.println("Removed:"+removed);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
