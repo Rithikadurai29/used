07d6cd900781739551b615940859b697d7529cc0
Rithika <durairithika26@gmail.com>
2026-06-23 10:37:01 +0530

    hello

---
+++ src/Day15.java
@@ -0,0 +1,19 @@
+ public class Dat15 {
+
+     static void main() throws InterruptedException {
+         Runnable chefTask = () -> {
+             String[] items = {"Biryani", "Parotta", "Noodles"};
+             for (int i = 0; i < items.length; i++) {
+                 System.out.println("Cooking Item:"+ items[i]);
+                 try {
+                     Thread.sleep(7000);
+                 } catch (InterruptedException e){
+                     throw new RuntimeException(e);
+                 }
+             }
+         };
+         Thread chefThread = new Thread(chefTask,"chef.thread");
+         chefThread.start();
+         chefThread.join();
+     }
+ }
